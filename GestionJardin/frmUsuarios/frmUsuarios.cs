64b08d65feef292a02e27e52e0a12df612adfc0c
Romina Merlo <rominamerlo07@gmail.com>
2020-09-22 19:37:44 -0300

    Se corrige codigo usuario

--- GestionJardin/frmUsuarios/frmUsuarios.cs
+++ GestionJardin/frmUsuarios/frmUsuarios.cs
@@ -19,32 +19,83 @@ namespace GestionJardin
             InitializeComponent();
             Settooltip();
             ObjetoUsu.AutocompletarenDocente(txtGU_Buscar);
-            CargarGrilla();
+            dgv_UsuariosActivos.DataSource = ObjetoUsu.MostrarUsu();
         }
 
         private void frmUsuarios_Load(object sender, EventArgs e)
         {
-          dgv_UsuariosActivos.ClearSelection();
-
-            //if (txtGU_Buscar.Text.Length > 0)
-            //{                 
-            //    dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(txtGU_Buscar.Text);
-            //}
-            //else 
-            //{
-            //    txtGU_Buscar.Clear();
-            //    dgv_UsuariosActivos.DataSource = ObjetoUsu.MostrarUsu();
-            //}
-            
+          dgv_UsuariosActivos.ClearSelection();                                  
         }
+            
+
+        /******  buscar datos Grilla ******/
+
+        private void txtGU_Buscar_ButtonClick(object sender, EventArgs e)
+        {
+            string docente = txtGU_Buscar.Text;
+            dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(docente);
+        }
+
+        private void txtGU_Buscar_Enter(object sender, EventArgs e)
+        {
+            string docente = txtGU_Buscar.Text;
+            dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(docente);
+        }
+
+        /******  Filtro Grilla ******/
+
+        private void txtGU_Buscar_TextChanged(object sender, EventArgs e)
+        {
+            if (txtGU_Buscar.Text.Length > 0)
+            {
+                dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(txtGU_Buscar.Text);
+            }
+            else
+            {
+                txtGU_Buscar.Clear();
+                dgv_UsuariosActivos.DataSource = ObjetoUsu.MostrarUsu();
+            }
+
+        }
+
+
+        /************* Mensaje evento hover boton  **************/
 
         private void Settooltip()
         {
             ToolTip Tip = new ToolTip();
-            Tip.SetToolTip(this.btnGU_Eliminar, "SELECCIONE UN REGISTRO PARA PODER ELIMINAR");
-            Tip.SetToolTip(this.btnGU_Agregar, "AGREGAR USUARIO");
+            Tip.SetToolTip(this.btnGU_Eliminar, "SELECCIONE UN REGISTRO PARA PODER ELIMINAR");           
             Tip.SetToolTip(this.btnGU_Editar, "SELECCIONE UN REGISTRO PARA PODER EDITAR");
         }
+
+
+        
+        /************* Habilitacion de botones segun seleccion de registro en grilla  **************/
+       
+
+        private void dgv_UsuariosActivos_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgv_UsuariosActivos.SelectedRows.Count > 0)
+            {
+                btnGU_Editar.IconColor = Color.Cyan;
+                btnGU_Editar.ForeColor = Color.Cyan;
+                btnGU_Eliminar.IconColor = Color.FromArgb(255, 128, 0);
+                btnGU_Eliminar.ForeColor = Color.FromArgb(255, 128, 0);
+            }
+            else
+            {
+                btnGU_Editar.IconColor = Color.Gray;
+                btnGU_Editar.ForeColor = Color.Gray;
+                btnGU_Eliminar.IconColor = Color.Gray;
+                btnGU_Eliminar.ForeColor = Color.Gray;                
+            }
+        }
+
+
+        /***************************************/
+        /*************** AGREGAR ***************/
+        /***************************************/
+
 
         private void btnGU_Agregar_Click(object sender, EventArgs e)
         {
@@ -55,6 +106,11 @@ namespace GestionJardin
             frmUsuariosPopUpAgregar.ShowDialog();
 
         }
+
+
+        /***************************************/
+        /*************** EDITAR ***************/
+        /***************************************/
 
         private void btnGU_Editar_Click(object sender, EventArgs e)
         {
@@ -80,6 +136,11 @@ namespace GestionJardin
 
         }
 
+
+        /****************************************/
+        /*************** ELIMINAR ***************/
+        /****************************************/
+
         private void btnGU_Eliminar_Click(object sender, EventArgs e)
         {
             frmUsuariosPopUpEliminar frmUsuariosPopUpEliminar = new frmUsuariosPopUpEliminar();
@@ -104,55 +165,6 @@ namespace GestionJardin
                 
             }
         }
-        public void CargarGrilla()
-        {
-            dgv_UsuariosActivos.DataSource = ObjetoUsu.MostrarUsu();
-
-        }
-
-        private void txtGU_Buscar_ButtonClick(object sender, EventArgs e)
-        {
-            string docente = txtGU_Buscar.Text;
-            dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(docente);
-        }
-
-        private void txtGU_Buscar_Enter(object sender, EventArgs e)
-        {
-            string docente = txtGU_Buscar.Text;
-            dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(docente);
-        }
-
-
-
-        private void dgv_UsuariosActivos_CellClick_1(object sender, DataGridViewCellEventArgs e)
-        {
-            if (dgv_UsuariosActivos.SelectedRows.Count > 0)
-            {
-                btnGU_Editar.IconColor = Color.Cyan;
-                btnGU_Editar.ForeColor = Color.Cyan;
-                btnGU_Eliminar.IconColor = Color.FromArgb(255, 128, 0);
-                btnGU_Eliminar.ForeColor = Color.FromArgb(255, 128, 0);
-            }
-            else
-            {
-                btnGU_Editar.IconColor = Color.Gray;
-                btnGU_Editar.ForeColor = Color.Gray;
-                btnGU_Eliminar.IconColor = Color.Gray;
-                btnGU_Eliminar.ForeColor = Color.Gray;                
-            }
-        }
-
-        private void txtGU_Buscar_TextChanged(object sender, EventArgs e)
-        {
-            if (txtGU_Buscar.Text.Length > 0)
-            {
-                dgv_UsuariosActivos.DataSource = ObjetoUsu.llenarGrilla(txtGU_Buscar.Text);
-            }
-            else
-            {
-                txtGU_Buscar.Clear();
-                dgv_UsuariosActivos.DataSource = ObjetoUsu.MostrarUsu();
-            }
-        }
+               
     }
 }
