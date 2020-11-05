a7f61b9d19e7d92a50be1efbb8e7612f38dcf2e7
Romina Merlo <rominamerlo07@gmail.com>
2020-11-05 10:57:30 -0300

    prueba 8521231691651

--- GestionJardin/frmSalas/frmSalas.cs
+++ GestionJardin/frmSalas/frmSalas.cs
@@ -15,7 +15,7 @@ namespace GestionJardin
 {
     public partial class frmSalas : Form
     {
-        //prueba 481523156
+        //prueba 8521231691651
         logSala2 metSala = new logSala2();
 
         public frmSalas()
